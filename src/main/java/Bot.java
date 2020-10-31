b972f0ca4c5a40461de0a63655dac58a7cfb45e4
Tarlan Ismayilsoy <tismayilsoy4578@ada.edu.az>
2020-10-31 20:38:56 +0400

    inital commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot
+{
+	@Override
+	public void onUpdateReceived(Update update) {
+		// We check if the update has a message and the message has text
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+					.setChatId(update.getMessage().getChatId())
+					.setText(update.getMessage().getText());
+			try {
+				execute(message); // Call method to send the message
+			} catch (TelegramApiException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public String getBotUsername() {
+		return System.getenv("BOT_USERNAME");
+	}
+	
+	@Override
+	public String getBotToken() {
+		return System.getenv("BOT_TOKEN");
+	}
+}
